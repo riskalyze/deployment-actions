ba62e87390209ce60784dfa2f5d6debf1d6acb93
Jesse Bye <jbye@riskalyze.com>
2022-06-02 10:47:15 -0700

    feat: Package helm chart then deploy in separate step

--- eks-helm-deploy/eks-helm-deploy.sh
+++ eks-helm-deploy/eks-helm-deploy.sh
@@ -2,7 +2,7 @@
 set -ueo pipefail
 
 if [ "$TASK" == "deploy" ]; then
-  params=("install" "$CHART_NAME" "$CHART_DIR" "-n" "$NAMESPACE" "--set" "cluster=$CLUSTER,environment=$ENVIRONMENT" "--wait")
+  params=("install" "$CHART_NAME" "./$CHART_NAME-*.tgz" "-n" "$NAMESPACE" "--set" "cluster=$CLUSTER,environment=$ENVIRONMENT" "--wait")
   kubectl apply -f - <<EOF
 apiVersion: v1
 kind: Namespace
@@ -18,9 +18,10 @@ elif [ "$TASK" == "destroy" ]; then
     echo "ERROR! Cannot destroy a production environment!!"
     exit 1
   else
-    params=("uninstall" "$CHART_NAME" "-n" "$NAMESPACE")
+    params=("uninstall" "$CHART_NAME" "-n" "$NAMESPACE" "--wait")
     echo Running helm "${params[@]}"
     helm "${params[@]}"
+    kubectl delete ns "$NAMESPACE"
   fi
 else
   echo "ERROR! Unrecognized action $TASK."
