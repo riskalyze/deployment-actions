551bcbbd7f76f08e8f1a9f24ba703658e11d76f3
Jesse Bye <jbye@riskalyze.com>
2022-07-01 11:00:44 -0700

    rename to tfplan

--- terraform/terraform.sh
+++ terraform/terraform.sh
@@ -6,14 +6,14 @@ if [ "$TASK" == "apply" ]; then
   terraform apply -auto-approve -input=false
 elif [ "$TASK" == "plan" ]; then
   echo "Running terraform plan..."
-  terraform plan -no-color -input=false -out=plan.out
+  terraform plan -no-color -input=false -out=tfplan
 elif [ "$TASK" == "destroy" ]; then
   if [[ "$ENVIRONMENT" =~ ^prod ]]; then
     echo "ERROR! Cannot destroy a production environment!!"
     exit 1
   else
     echo "Running terraform destroy..."
-    terraform apply -destroy -auto-approve -input=false -lock-timeout=30s
+    terraform apply -destroy -auto-approve -input=false -lock-timeout=30s tfplan
   fi
 else
   echo "ERROR! Unrecognized action $TASK."
