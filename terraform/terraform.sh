7577ee663bc4973ebbc57a34832b95d1ed070e2f
Jesse Bye <jbye@riskalyze.com>
2022-07-01 12:10:08 -0700

    include vars file

--- terraform/terraform.sh
+++ terraform/terraform.sh
@@ -3,17 +3,17 @@ set -ueo pipefail
 
 if [ "$TASK" == "apply" ]; then
   echo "Running terraform apply..."
-  terraform apply -auto-approve -input=false
+  terraform apply -var-file=$ENVIRONMENT.tfvars -auto-approve -input=false
 elif [ "$TASK" == "plan" ]; then
   echo "Running terraform plan..."
-  terraform plan -no-color -input=false -out=tfplan
+  terraform plan -no-color -input=false -var-file=$ENVIRONMENT.tfvars -out=tfplan
 elif [ "$TASK" == "destroy" ]; then
   if [[ "$ENVIRONMENT" =~ ^prod ]]; then
     echo "ERROR! Cannot destroy a production environment!!"
     exit 1
   else
     echo "Running terraform destroy..."
-    terraform apply -destroy -auto-approve -input=false -lock-timeout=30s tfplan
+    terraform apply -destroy -var-file=$ENVIRONMENT.tfvars -auto-approve -input=false -lock-timeout=30s tfplan
   fi
 else
   echo "ERROR! Unrecognized action $TASK."
